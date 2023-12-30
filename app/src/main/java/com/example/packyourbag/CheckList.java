85cd205b0a3ae147ec7c8ece59d6c5e765941db6
altafhussain786 <altafhussainhussain09@gmail.com>
2023-12-30 22:06:03 +0500

    new updates

--- app/src/main/java/com/example/packyourbag/CheckList.java
+++ app/src/main/java/com/example/packyourbag/CheckList.java
@@ -5,12 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.packyourbag.Adapter.CheckListAdapter;
+import com.example.packyourbag.Constants.MyConstants;
 import com.example.packyourbag.Database.RoomDB;
 import com.example.packyourbag.Models.Items;
 
@@ -33,12 +37,69 @@ public class CheckList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent=getIntent();
+        header =intent.getStringExtra(MyConstants.HEADER_SMALL);
+        show=intent.getStringExtra(MyConstants.SHOW_SMALL);
+
+        getSupportActionBar().setTitle(header);
+        txtAdd=findViewById(R.id.txtAdd);
+        btnAdd=findViewById(R.id.btnAdd);
+        recyclerView=findViewById(R.id.recyclerView);
+        linearLayout=findViewById(R.id.linearLayout);
+
+        database=RoomDB.getInstance(this);
+        if (MyConstants.FALSE_STRING.equals(show)){
+            linearLayout.setVisibility(View.GONE);
+            itemsList=database.mainDao().getAllSelected(true);
+
+        }else{
+            itemsList=database.mainDao().getAll(header);
+        }
+updateRecyler(itemsList);
+
+//        add new item
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName=txtAdd.getText().toString();
+                if (itemName!=null && !itemName.isEmpty()){
+                    addNewItem(itemName);
+                    Toast.makeText(CheckList.this, "added item sucessfullly", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(CheckList.this, "Empy ccan not be added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+       onBackPressed();
+       return true;
+
+    }
+
+    private void addNewItem(String itemName){
+        Items item=new Items();
+        item.setChecked(false);
+        item.setCategory(header);
+        item.setItemname(itemName);
+        item.setAddedby(MyConstants.USER_SMALL);
+        database.mainDao().saveItem(item);
+        itemsList=database.mainDao().getAll(header);
+        updateRecyler(itemsList);
+        recyclerView.scrollToPosition(checkListAdapter.getItemCount()-1);
+        txtAdd.setText("");
     }
 
     private void updateRecyler(List<Items> itemsList){
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
         checkListAdapter=new CheckListAdapter(CheckList.this,itemsList,database,show);
+        recyclerView.setAdapter(checkListAdapter);
 
 
 
